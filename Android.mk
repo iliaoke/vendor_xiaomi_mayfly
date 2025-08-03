522c761f909e3a5edf5691300331a64fad8edff2
liaoke <liaoke2007@gamil.com>
2025-08-03 14:25:37 +0800

    fix

--- Android.mk
+++ Android.mk
@@ -23,7 +23,7 @@ $(call add-radio-file-sha1-checked,radio/shrm.img,5c9ef3d78994ec96d8d6100e8f3bea
 $(call add-radio-file-sha1-checked,radio/tz.img,2c1801ba76685e51f7ff52e31bf160abd01a8904)
 $(call add-radio-file-sha1-checked,radio/uefi.img,fcd79054e9c1b519ef1b150d9c059cad9ee34839)
 $(call add-radio-file-sha1-checked,radio/uefisecapp.img,e9c00e858f99984790829074a05809321df28915)
-$(call add-radio-file-sha1-checked,radio/vm-bootsys.img,e6a8f02da16f43d1cedb81d09ed6aa9b5230ab0)
+$(call add-radio-file-sha1-checked,radio/vm-bootsys.img,e6a8f02da16f43d1dcedb81d09ed6aa9b5230ab0)
 $(call add-radio-file-sha1-checked,radio/xbl.img,7d51ca13a8b7d363e64d0f034bd96bbf44b536ba)
 $(call add-radio-file-sha1-checked,radio/xbl_config.img,9d211b6f0b83ae2d3a51127f129fc7d8de44b1e6)
 $(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,4631bfa0a6ae2722996d3b539f6323e3ba3be8c0)
