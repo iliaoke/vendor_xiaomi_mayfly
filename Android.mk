2ee05975efe025d201d0d81a03ba826311d7a5a5
liaoke <liaoke2007@gamil.com>
2025-08-03 15:43:18 +0800

    fix

---
+++ Android.mk
@@ -0,0 +1,32 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),mayfly)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,2056c9ee2944736d5e719b06a6ec96aadcb4ebb4)
+$(call add-radio-file-sha1-checked,radio/aop.img,f9201c0985e329911eb58143cc83c53059e3da22)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,e3d7fa2bfb1e555353ec27224c97a6a2a7f6b9f0)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,a463eda42d1945d26a6d5d165bdc90fc39003673)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,c66fdbcaf63c352147359bd72ce5ef2d5b39b39f)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,1375a66077de2e21e689dc95a0581ac3ecdc1507)
+$(call add-radio-file-sha1-checked,radio/dsp.img,259329c9822fa19d5ba7f6e06794c44ceec3d220)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,b98789483ba86c727c08e70f337d5ef7d3682bde)
+$(call add-radio-file-sha1-checked,radio/hyp.img,37df11ece4fdb9a855b78175db9bcda08cb72659)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,6eba22e749d9b38f262bb1981d7bcd7c20b7a1a0)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,a3fec9f1c0ed73789ae3e1261ae58d7cd9e3d6db)
+$(call add-radio-file-sha1-checked,radio/modem.img,da1e1e996bb3ad0f6b5799a63ddf7885d21cb6ed)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,23ebaad5ed06f30021b4c89071d23b151a6ef1a3)
+$(call add-radio-file-sha1-checked,radio/shrm.img,9a095edbfd66ed30e10bdf2ee11a7aa9586b75e6)
+$(call add-radio-file-sha1-checked,radio/tz.img,9f0c4f3379ddb263b661a3998b36fa11e109d151)
+$(call add-radio-file-sha1-checked,radio/uefi.img,77dee7d041cc8b1436e56de93922869419daa542)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,3e61b8e8db09d6137953fa5e68ed5737ac0f5968)
+$(call add-radio-file-sha1-checked,radio/vm-bootsys.img,ba20165ced28dca887728c18dedd4135ad492976)
+$(call add-radio-file-sha1-checked,radio/xbl.img,6797691b12a96fa2cff876a3f5b4344b68e47601)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,8edf63f5db8ded1d03dcbb4fd97bd27cb070431d)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,98d81598c286f95ad0c37c07ceb7ed70840dc87b)
+
+endif
+
